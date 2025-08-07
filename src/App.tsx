8e8c9657b6a171113c194d36d7d2dfad5decafab
ThanhluanGif <nluan5517@gmail.com>
2025-08-07 14:38:56 +0700

    Add ColorChange component and integrate into App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorChange from './ColorChange';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <ColorChange />
+    </div>
+  );
+};
+
+export default App;
